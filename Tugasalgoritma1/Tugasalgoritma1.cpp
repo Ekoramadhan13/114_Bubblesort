d83df9982fd4a8a88983279ecb9d93e4b92dc031
Ekoramadhan13 <enugraha411@gmail.com>
2024-03-08 10:59:17 +0700

    Add project files.

---
+++ Tugasalgoritma1/Tugasalgoritma1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "==============" << endl; 
+	cout << "==============" << endl;
+	cout << "==============" << endl;
+
+
+   
